1e6720234475110d13b2fdd5db2b46fe44d0788e
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-15 13:44:17 +0100

    lesson on metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: {
+      absolute: "My Blog",
+   },
+};
+
 const Blog = () => {
    return <h1>My Blog website</h1>;
 };
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,23 @@
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+import { Metadata } from "next";
+
+type Props = {
+   params: { productId: string };
+};
+
+export const generateMetadata = async ({
+   params,
+}: Props): Promise<Metadata> => {
+   const title = await new Promise((resolve) => {
+      setTimeout(() => {
+         resolve(`Iphone ${params.productId}`);
+      }, 100);
+   });
+   return {
+      title: `Product ${title}`,
+   };
+};
+
+const ProductDetails = ({ params }: Props) => {
    return (
       <div className="">
          <h1>Product details about {params.productId}</h1>
