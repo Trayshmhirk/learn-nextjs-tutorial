eb4a67462a8a9ca54eeb0210d0dfe1b0dbe9f211
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-17 11:16:02 +0100

    lessons on redirects, headers, cookies and caching in route handlers

--- route-handler-demo/src/app/profile/api/route.ts
+++ route-handler-demo/src/app/profile/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-   return new Response("Profile Data!");
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+   const requestHeaders = new Headers(request.headers);
+   const headerList = headers();
+
+   (await cookies()).set("resultsPerPage", "20");
+   console.log((await cookies()).get("resultsPerPage"));
+
+   const theme = request.cookies.get("theme");
+   console.log(theme);
+
+   console.log(requestHeaders.get("Authorization"));
+   console.log((await headerList).get("Authorization"));
+
+   return new Response("<h1>Profile Data!</h1>", {
+      headers: {
+         "Content-Type": "text/html",
+         "SET-Cookie": "theme=dark",
+      },
+   });
 }
